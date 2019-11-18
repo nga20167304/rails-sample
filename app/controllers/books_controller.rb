30ea4e0a8dd41131f63f3b912d81e37c3cba1fee
EC2 Default User <ec2-user@ip-172-31-42-129.ec2.internal>
2019-11-18 23:35:58 +0000

    ruby week9 homework

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,6 @@
 class BooksController < ApplicationController
   before_action :set_book, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user! #phai dang nhap truoc thi moi xem duoc book
 
   # GET /books
   # GET /books.json
